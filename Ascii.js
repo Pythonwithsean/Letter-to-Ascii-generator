c221696809a54b451193094f4c469d08c51da1ef
Sean <107402787+Lunafcs@users.noreply.github.com>
2023-09-04 12:00:17 +0100

    Add files via upload

---
+++ Ascii.js
@@ -0,0 +1,14 @@
+// let asciiNumber = Math.floor(Math.random() * (122 - 97 + 1)) + 97;
+// let asciiValue = String.fromCharCode(asciiNumber)
+// console.log(asciiValue)
+
+function letterToAscii(l){
+   let result = ""
+   for (let i = 0; i< l.length; i++){
+      result += l.charCodeAt(i) + "|"
+   }
+   console.log("Your Letters in Ascii are: " + result )
+}
+
+letterToAscii("#")
+
