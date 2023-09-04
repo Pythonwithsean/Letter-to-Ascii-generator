126a8d97b1b7828324e558555ca4806078308a12
Sean <107402787+Lunafcs@users.noreply.github.com>
2023-09-04 12:01:53 +0100

    Update Ascii.js

--- Ascii.js
+++ Ascii.js
@@ -1,7 +1,11 @@
-// let asciiNumber = Math.floor(Math.random() * (122 - 97 + 1)) + 97;
-// let asciiValue = String.fromCharCode(asciiNumber)
-// console.log(asciiValue)
 
+// From Ascii to Character
+let asciiNumber = Math.floor(Math.random() * (122 - 97 + 1)) + 97;
+let asciiValue = String.fromCharCode(asciiNumber)
+console.log(asciiValue)
+
+
+//From Letter to ascii
 function letterToAscii(l){
    let result = ""
    for (let i = 0; i< l.length; i++){
